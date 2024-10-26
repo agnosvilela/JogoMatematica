4edd7c74dc5894d9236c1776ebbdd7cb91de76cf
Agnos Vilela <94070018+agnosvilela@users.noreply.github.com>
2024-10-26 16:38:18 -0300

    Add files via upload

---
+++ jogomatematica/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace jogomatematica
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // variaveis para armazenar a quantidade de questoes que o usuario quer responder.
+            int quantidadeQuestoes;
+
+            // armazena a quantidade de resposta que o usuario acertou
+            int respostasCorretas = 0;
+
+            QuestoesMatematica questoes = new QuestoesMatematica();
+            Console.WriteLine("Jogo de Matematica \n");
+            Console.WriteLine("Digite a quantidade de questoes : ");
+            quantidadeQuestoes = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadeQuestoes; i++)
+            {
+                bool acertou = questoes.GerarQuestaoAdicao();
+                if(acertou)
+                {
+                    respostasCorretas++;
+                }
+
+                Console.WriteLine("\nvocê acertou a resposta de " +respostasCorretas + " de " + quantidadeQuestoes);
+                Console.WriteLine("********************************");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ jogomatematica/QuestoesMatematica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogomatematica
+{
+    public class QuestoesMatematica
+    {
+        // propiedade responsavel por gerar números aleatorios.
+        private Random random = new Random();
+        public bool GerarQuestaoAdicao()
+        
+        {
+            bool acertou;
+            int numero1 = random.Next(51);
+            int numero2 = random.Next(51);
+            Console.WriteLine(numero1 + "+" + numero2 + " = ");
+            int resposta = Int32.Parse(Console.ReadLine());
+
+            // calculando a resposta correta
+            
+            int resultado = numero1 + numero2;
+            if(resultado == resposta)
+            {
+                acertou = true;
+            }
+            else
+            {
+                acertou = false;
+            }
+            return acertou;
+        }
+    }
+}
